66307d8eb50f9eeddba45f4b68c764f090894555
Arman Akbarian <arman2@gmail.com>
2016-05-11 00:26:23 -0700

    acp

--- _posts/bin/mydate2.py
+++ _posts/bin/mydate2.py
@@ -8,11 +8,24 @@ import os.path
 
 import sys
 
+
+def parse_textfile_todict(fname):
+    fdict = {}
+    try:
+        with open(fname,"r") as f:
+            for line in f:
+                a, b = line.split()
+                fdict[a] = b
+    except:
+        fdict = {}
+    return fdict
+
 try:
-    file = sys.argv[1]
-    lm = os.path.getmtime(file)
+    all_dates  = parse_textfile_todict('post_dates.param')
+    fname = sys.argv[1]
+    lm = os.path.getmtime(fname)
     lm = datetime.fromtimestamp(lm)
-    print(lm.strftime('%Y-%m-%d'))
+    print(all_dates.get(fname,lm.strftime('%Y-%m-%d')))
 
 except: 
     print('')
